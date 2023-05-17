90ede2036202fc0955051db8b0c8db999b79b888
David Kim <dhskim22@gmail.com>
2023-05-17 00:54:44 -0400

    finish user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,8 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
+
 
 
 class User(db.Model, UserMixin):
@@ -10,9 +12,18 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    name = db.Column(db.String(40), nullable=False)
+    display_name = db.Column(db.String(40), nullable=True)
+    name_pronunciation = db.Column(db.String(40), nullable=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    status = db.Column(db.String(100), nullable=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    profile_pic = db.Column(db.String(100), nullable=True)
+    title = db.Column(db.String(50), nullable=True)
+    phone_number = db.Column(db.String(50), nullable=True)
+    timezone = db.Column(db.String(50), nullable=True)
+    created_at = db.Column(db.Date, default=datetime.today)
+    updated_at = db.Column(db.Date, default=datetime.today)
 
     @property
     def password(self):
@@ -28,6 +39,16 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'name': self.name,
+            'display_name': self.display_name,
+            'name_pronunciation': self.name_pronunciation,
+            'email': self.email,
+            'status': self.status,
+            'hashed_password': self.hashed_password,
+            'profile_pic': self.profile_pic,
+            'title': self.title,
+            'phone_number': self.phone_number,
+            'timezone': self.timezone,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
