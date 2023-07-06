a83204fb5a8e65400bb9044c3b772a2e4e840503
David Kim <dhskim22@gmail.com>
2023-07-06 10:46:11 -0400

    implememnt caousel

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -2,11 +2,48 @@ import React from 'react';
 import './Homepage.css';
 import { useModal } from '../../context/Modal';
 import LoginFormModal from '../LoginFormModal';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 function Homepage() {
 
     const { setModalContent } = useModal();
     // console.log(modal)
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 1
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+        // desktop: {
+        //     breakpoint: { max: 3000, min: 1024 },
+        //     items: 1,
+        //     slidesToSlide: 1 // optional, default to 1.
+        // },
+        // tablet: {
+        //     breakpoint: { max: 1024, min: 464 },
+        //     items: 1,
+        //     slidesToSlide: 1 // optional, default to 1.
+        // },
+        // mobile: {
+        //     breakpoint: { max: 464, min: 0 },
+        //     items: 1,
+        //     slidesToSlide: 1 // optional, default to 1.
+        // }
+    };
 
     return (
         <>
@@ -20,29 +57,45 @@ function Homepage() {
                     </p>
                 </div>
                 <div className='home-body'>
-                    <div className='feature-carousel'>
+                        {/* <Carousel responsive={responsive}> */}
                         <h1>Take a look at the features!</h1>
-                        <div className='single-feature'>
-                            <h3>Workspace Feature</h3>
-                            <p>Having multiple workspaces to be able to maintain different teams. Creating a new workspace with the ablilty to invite users.</p>
-                            <img id='workspace-feature-img' src={'../../../../static/all-workspaces.PNG'} style={{ width: '450px' }}></img>
-                        </div>
-                        <div className='single-feature'>
-                            <h3>Channel Feature</h3>
-                            <p>The channel feature gives users the ability to have different topics and handle their converstations within the same workspace.</p>
-                            <img src={'../../../../static/channel-feature-img.PNG'} style={{ width: '450px' }}></img>
-                        </div>
-                        <div className='single-feature'>
-                            <h3>Message Feature</h3>
-                            <p>The message feature utilizes web-sockets so users are able to communicate in real-time</p>
-                            <img src={'../../../../static/message-feature-img.PNG'} style={{ width: '450px' }}></img>
-                        </div>
-                        <div className='single-feature'>
-                            <h3>Thread Message Feature</h3>
-                            <p>The thread message feature lets users respond to a specific message to keep the channel </p>
-                            <img src={'../../../../static/thread-message-feature-img.PNG'} alt='Feature coming soon!' style={{ width: '450px' }}></img>
-                        </div>
-                    </div>
+                        <Carousel
+                            swipeable={false}
+                            draggable={false}
+                            showDots={true}
+                            responsive={responsive}
+                            ssr={true} // means to render carousel on server-side.
+                            infinite={true}
+                            autoPlaySpeed={5000}
+                            keyBoardControl={true}
+                            customTransition="all .5"
+                            transitionDuration={500}
+                            containerClass="carousel-container"
+                            removeArrowOnDeviceType={["tablet", "mobile"]}
+                            dotListClass="custom-dot-list-style"
+                            itemClass="carousel-item-padding-40-px"
+                        >
+                            <div className='single-feature'>
+                                <h3>Workspace Feature</h3>
+                                <p>Having multiple workspaces to be able to maintain different teams. Creating a new workspace with the ablilty to invite users.</p>
+                                <img id='workspace-feature-img' src={'../../../../static/all-workspaces.PNG'} style={{ width: '450px' }}></img>
+                            </div>
+                            <div className='single-feature'>
+                                <h3>Channel Feature</h3>
+                                <p>The channel feature gives users the ability to have different topics and handle their converstations within the same workspace.</p>
+                                <img src={'../../../../static/channel-feature-img.PNG'} style={{ width: '450px' }}></img>
+                            </div>
+                            <div className='single-feature'>
+                                <h3>Message Feature</h3>
+                                <p>The message feature utilizes web-sockets so users are able to communicate in real-time</p>
+                                <img src={'../../../../static/message-feature-img.PNG'} style={{ width: '450px' }}></img>
+                            </div>
+                            <div className='single-feature'>
+                                <h3>Thread Message Feature</h3>
+                                <p>The thread message feature lets users respond to a specific message to keep the channel </p>
+                                <img src={'../../../../static/thread-message-feature-img.PNG'} alt='Feature coming soon!' style={{ width: '450px' }}></img>
+                            </div>
+                        </Carousel>
                     {/* <div className='getting-started'>
                         <h1>Ready to get Started!</h1>
                         <div className='launch-NotTaut' onClick={setModalContent(<LoginFormModal />)}>
